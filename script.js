81aab2ff30258bcc175eb8afa7cf6f360889988e
AungPyaeSoneUCS <mgaungpyaesone98@gmail.com>
2026-05-21 18:11:43 +0630

    Update May 21

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   initNrcBoxes();
   initAcademicFields();
   initPhotoUpload();
+  initExamHistoryFields();
 
   const form = document.getElementById("admissionForm");
 
