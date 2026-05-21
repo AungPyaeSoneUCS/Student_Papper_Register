cb16c0900f3c1ed1a90d21017e19e6ec598021a0
AungPyaeSoneUCS <mgaungpyaesone98@gmail.com>
2026-05-21 18:26:52 +0630

    Update May 21

--- second/index.js
+++
@@ -1,9 +0,0 @@
-// file: second/index.js
-document.addEventListener("DOMContentLoaded", () => {
-  const data = JSON.parse(sessionStorage.getItem("studentFormData")) || {};
-
-  document.getElementById("showCourseYear").textContent = data.courseYear || "-";
-  document.getElementById("showSemester").textContent = data.semester || "-";
-  document.getElementById("showMajor").textContent = data.major || "-";
-  document.getElementById("showRollNo").textContent = data.rollNo || "-";
-});
